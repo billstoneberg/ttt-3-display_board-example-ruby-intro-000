8bbdb9feeec2dedff03b7d9593afa431c2e82923
William Stoneberg <wrstoneberg@me.com>
2020-09-14 11:11:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
-def display_board
+board = [" "," "," "," "," "," "," "," "," "]
+
+display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-end
-display_board
 
 
 
